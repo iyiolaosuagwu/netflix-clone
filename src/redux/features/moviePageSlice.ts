bddf4176da1d22367531ce763d39bd7dee9ccf2b
Iyiolaosuagwu <iyiolaosuagwu1@gmail.com>
2023-04-25 01:02:14 +0100

    fix: api key

--- src/redux/features/moviePageSlice.ts
+++ src/redux/features/moviePageSlice.ts
@@ -1,65 +1,79 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { fetchData } from "../../../utils/requests";
 
-
-
-const initialState:any = {
+const initialState: any = {
     headerShows: [],
     sectionOne: [],
-    sectionTwo:[],
-    sectionThree:[]
-}
+    sectionTwo: [],
+    sectionThree: [],
+};
 export const getMovieHeaderShows: any = createAsyncThunk(
-    'moviePageShows/getHeaderShows',
+    "moviePageShows/getHeaderShows",
     async (dispatch, getState) => {
-      const res = await fetchData('https://api.themoviedb.org/3/movie/top_rated', process.env.NEXT_PUBLIC_API_KEY, 'GET')
-      return res.results
-  })
-  export const getMovieSectionOne: any = createAsyncThunk(
-    'moviePageShows/getMovieSectionOne',
+        const res = await fetchData(
+            "https://api.themoviedb.org/3/movie/top_rated",
+            process.env.NEXT_PUBLIC_API_KEY,
+            "GET"
+        );
+        return res.results;
+    }
+);
+export const getMovieSectionOne: any = createAsyncThunk(
+    "moviePageShows/getMovieSectionOne",
     async (dispatch, getState) => {
-      const res = await fetchData('https://api.themoviedb.org/3/movie/now_playing', process.env.NEXT_PUBLIC_API_KEY, 'GET')
-      return res.results
-  })
-  export const getMovieSectionTwo: any = createAsyncThunk(
-    'moviePageShows/getMovieSectionTwo',
+        const res = await fetchData(
+            "https://api.themoviedb.org/3/movie/now_playing",
+            process.env.NEXT_PUBLIC_API_KEY,
+            "GET"
+        );
+        return res.results;
+    }
+);
+export const getMovieSectionTwo: any = createAsyncThunk(
+    "moviePageShows/getMovieSectionTwo",
     async (dispatch, getState) => {
-      const res = await fetchData('https://api.themoviedb.org/3/movie/popular', process.env.NEXT_PUBLIC_API_KEY, 'GET')
-      return res.results
-  })
+        const res = await fetchData(
+            "https://api.themoviedb.org/3/movie/popular",
+            process.env.NEXT_PUBLIC_API_KEY,
+            "GET"
+        );
+        return res.results;
+    }
+);
 const movieSlice = createSlice({
     name: "moviePageShows",
     initialState,
     reducers: {},
     extraReducers: {
         [getMovieHeaderShows.pending]: (state) => {
-            state.headerShows = []
+            state.headerShows = [];
         },
         [getMovieHeaderShows.fulfilled]: (state, action) => {
-            state.headerShows = action.payload
+            state.headerShows = action.payload;
         },
         [getMovieHeaderShows.rejected]: (state) => {
-            state.headerShows = []
+            state.headerShows = [];
         },
         [getMovieSectionOne.pending]: (state) => {
-            state.sectionOne = []
+            state.sectionOne = [];
         },
         [getMovieSectionOne.fulfilled]: (state, action) => {
-            state.sectionOne = action.payload
+            state.sectionOne = action.payload;
         },
         [getMovieSectionOne.rejected]: (state) => {
-            state.sectionOne = []
+            state.sectionOne = [];
         },
         [getMovieSectionTwo.pending]: (state) => {
-            state.sectionTwo = []
+            state.sectionTwo = [];
         },
         [getMovieSectionTwo.fulfilled]: (state, action) => {
-            state.sectionTwo = action.payload
+            state.sectionTwo = action.payload;
         },
         [getMovieSectionTwo.rejected]: (state) => {
-            state.sectionTwo = []
+            state.sectionTwo = [];
         },
-    }
-})
+    },
+});
 
 export default movieSlice.reducer;
+// hh
